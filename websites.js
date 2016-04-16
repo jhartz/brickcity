183ca8112f4461402c6dad66a1b5d00a7826bd66
Jake Hartz <jhartz@outlook.com>
2016-04-15 23:44:12 -0400

    Initial commit

---
+++ websites.js
@@ -0,0 +1,58 @@
+var title = "Story Maps";
+var subtitle = "Brick City";
+var showControlWindowButton = false;
+var showFullScreenButton = true;
+
+var websites = [
+    {
+        title: "Places To Visit Before They Disappear",
+        creator: "A Student",
+        url: "http://bit.ly/1CHZp8H"
+    },
+    {
+        title: "My Journey Across the United States",
+        creator: "Another Student",
+        url: "http://bit.ly/1tlKYA9"
+    },
+    {
+        title: "Major Cities within the Tech Industry",
+        creator: "A Third Student",
+        url: "https://ritarcgis.maps.arcgis.com/apps/presentation/index.html?webmap=b8e120ec5f714ff5ac4c7a52a61ccc3d"
+    },
+    {
+        title: "Historic Points near Oley, PA",
+        creator: "Jake Hartz",
+        url: "http://jhartz.github.io/maps/OleyHistoric/"
+    },
+    {
+        title: "Tour of Pittsburg",
+        creator: "A Fifth Student",
+        url: "http://bit.ly/1mWzVAt"
+    },
+    {
+        title: "Ebola Outbreak Map",
+        creator: "A Sizth Student",
+        url: "https://ritarcgis.maps.arcgis.com/apps/presentation/index.html?webmap=4239203abfc84168a4459295c8b7fb57",
+        alwaysRefresh: true
+    },
+    {
+        title: "Air Pollution",
+        creator: "A Seventh Student",
+        url: "http://bit.ly/1xH2bec"
+    },
+    {
+        title: "Summer Vacation to Canada",
+        creator: "An Eighth Student",
+        url: "http://bit.ly/YUQwd0"
+    },
+    {
+        title: "Malmö",
+        creator: "Another Student",
+        url: "http://www.arcgis.com/apps/presentation/index.html?webmap=a4cdd5ff897a475c9e0abc8793725827"
+    },
+    {
+        title: "Nordic Nations",
+        creator: "Yet Another Student",
+        url: "http://bit.ly/10m3q4P"
+    }
+];
